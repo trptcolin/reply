ec857e3ec416330789910b48cb508d91d5cdfb74
Colin Jones <trptcolin@gmail.com>
2011-10-08 12:25:16 -0500

    Support basic readline cancellation via ctrl-c

--- src/reply/core.clj
+++ src/reply/core.clj
@@ -1,25 +1,52 @@
 (ns reply.core
-  (:use [clojure.main :only [repl]])
+  (:use [clojure.main :only [repl repl-read]]
+        [clojure.repl :only [set-break-handler!]])
   (:import [scala.tools.jline.console ConsoleReader]
-           [java.io PrintWriter]))
+           [java.io File
+                    PrintWriter]))
+
+(def input-getter (atom (future)))
+
+(defn handle-ctrl-c [signal]
+  (println "^C")
+  (future-cancel @input-getter))
+
+(defn jline-read [request-prompt request-exit]
+  (try
+    (let [future-input (future
+                         (try
+                           (let [reader (ConsoleReader.)]
+                             (doto reader
+                               (.setBellEnabled false)
+                               (.setPrompt (format "%s=> " (ns-name *ns*))))
+                             (.readLine reader))
+                           (catch Throwable e
+                             (print "caught throwable in readLine: ")
+                             (prn e)
+                             nil)))
+          _ (reset! input-getter future-input)
+          input (deref future-input)
+          ]
+      (cond (not input)
+              request-exit
+            (every? #(Character/isWhitespace %) input)
+              request-prompt
+            :else
+              (read (java.io.PushbackReader. (java.io.StringReader. input)))))
+    (catch Throwable e
+      request-prompt)))
 
 (defn -main [& args]
-  (let [reader (doto (ConsoleReader.)
-                 (.setBellEnabled false)
-                 (.setPrompt ""))
-        out (-> reader
-                (.getOutput)
-                (PrintWriter.))
-        jline-read (fn [request-prompt request-exit]
-                     (let [input (.readLine reader)]
-                       (cond (not input)
-                               request-exit
-                             (every? #(Character/isWhitespace %) input)
-                               request-prompt
-                             :else
-                               (read-string input))))
-        jline-prompt (fn [] (.setPrompt reader
-                                        (format "%s=> " (ns-name *ns*))))]
-    (binding [*out* out]
+  (set-break-handler! handle-ctrl-c)
+    (try
+
       (repl :read jline-read
-            :prompt jline-prompt))))
+            :prompt (constantly ""))
+
+      ; TODO: surely we can avoid this - not sure what's keeping the jvm alive.
+      (System/exit 0)
+
+      (catch Throwable e
+        (print "wrapping repl call: " )
+        (prn e))))
+
