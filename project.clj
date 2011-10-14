177f5787783f33187683e248f62e2b404edf7cd4
Colin Jones <trptcolin@gmail.com>
2011-10-13 22:49:01 -0500

    Improve docs, increment to 0.0.2 to signify the reasonably working state

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject reply "0.0.1-SNAPSHOT"
-  :description "REPL-y: a better Clojure REPL"
+(defproject reply "0.0.2-SNAPSHOT"
+  :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.scala-lang/jline "2.9.1"]]
   :aot [reply.JlineInputReader]
