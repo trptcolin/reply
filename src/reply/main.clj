cdee63066ed65d9be6b6fcef44684992ad949634
Colin Jones <trptcolin@gmail.com>
2012-01-09 08:08:47 -0600

    Isolate JLine dependency from main
    Now in just 2 publicly-used fns, reset-reader and read, in a
    new ns. Switching readers is as easy as defining new fns.
    Maybe this should be a protocol?

--- src/reply/main.clj
+++ src/reply/main.clj
@@ -1,42 +1,11 @@
 (ns reply.main
-  (:use [clojure.main :only [repl repl-read repl-exception]]
+  (:use [clojure.main :only [repl repl-exception]]
         [clojure.repl :only [set-break-handler!]])
   (:require [reply.hacks.printing :as hacks.printing]
-            [reply.completion.jline :as completion.jline]
-            [clojure.string :as str])
-  (:import [reply JlineInputReader]
-           [reply.hacks CustomizableBufferLineNumberingPushbackReader]
-           [scala.tools.jline.console ConsoleReader]
-           [scala.tools.jline.console.history FileHistory]
-           [scala.tools.jline.internal Log]
-           [java.io File IOException]))
-
-(defn actual-read [input-reader request-prompt request-exit]
-  (binding [*in* input-reader]
-    (repl-read request-prompt request-exit)))
+            [reply.reader.jline :as reader.jline]
+            [clojure.string :as str]))
 
 (def main-thread (Thread/currentThread))
-
-(defn make-reader []
-  (let [reader (ConsoleReader.)
-        home (System/getProperty "user.home")
-        history (FileHistory. (File. home ".jline-reply.history"))
-        completer (completion.jline/make-var-completer 'clojure.core)
-        completion-handler (completion.jline/make-completion-handler)]
-    (doto reader
-      (.setBellEnabled false)
-      (.setHistory history)
-      (.setCompletionHandler completion-handler)
-      (.addCompleter completer))))
-
-(def jline-reader (atom nil))
-
-(defn get-prompt [ns]
-  (format "%s=> " (ns-name ns)))
-
-(defn clear-jline-buffer []
-  (-> (.getCursorBuffer @jline-reader)
-      (.clear)))
 
 (def evaling-line (atom nil))
 (def printing-line (atom nil))
@@ -51,35 +20,6 @@
         (println "; Sorry, have to call Thread.stop on this command, because it's not dying.")
         (println ";;;;;;;;;;")
         (.stop thread)))))
-
-(defn set-empty-prompt []
-  (.setPrompt
-    @jline-reader
-    (apply str
-           (concat (repeat (- (count (get-prompt *ns*)) 2) \space)
-                   [\| \space]))))
-
-(def jline-pushback-reader (atom nil))
-
-(defn jline-read [request-prompt request-exit]
-  (reset! evaling-line nil)
-  (reset! printing-line nil)
-  (try
-    (.setPrompt @jline-reader (get-prompt *ns*))
-    (let [completer (first (.getCompleters @jline-reader))]
-      (.removeCompleter @jline-reader completer)
-      (.addCompleter @jline-reader (completion.jline/make-var-completer *ns*)))
-    (let [input-stream @jline-pushback-reader]
-      (do
-        (Thread/interrupted) ; just to clear the status
-        (actual-read input-stream request-prompt request-exit)))
-    ; NOTE: this indirection is for wrapped exceptions in 1.3
-    (catch Throwable e
-      (if (#{IOException InterruptedException} (type (repl-exception e)))
-        (do
-          (clear-jline-buffer)
-          request-prompt)
-        (throw e)))))
 
 (defn act-in-future [form action-atom base-action]
   (try
@@ -107,31 +47,22 @@
   (stop printing-line)
   (stop evaling-line :hard-kill-allowed true)
 
-  (clear-jline-buffer))
-
-(defn setup-reader! []
-  (Log/setOutput (java.io.PrintStream. (java.io.ByteArrayOutputStream.)))
-  (reset! jline-reader (make-reader)) ; since construction is side-effect-y
-  (reset! jline-pushback-reader ; since this depends on jline-reader
-    (CustomizableBufferLineNumberingPushbackReader.
-      (JlineInputReader.
-        {:jline-reader @jline-reader
-         :set-empty-prompt set-empty-prompt})
-      1)))
+  (reader.jline/reset-reader))
 
 (defn -main [& args]
   (set-break-handler! handle-ctrl-c)
-  (setup-reader!)
+;  (setup-reader!)
   (println "Clojure" (clojure-version))
 
   (with-redefs [clojure.core/print-sequential hacks.printing/print-sequential]
-    (repl :read jline-read
+    (repl :read (fn [prompt exit]
+                  (reset! evaling-line nil)
+                  (reset! printing-line nil)
+                  (reader.jline/read prompt exit))
           :eval reply-eval
           :print reply-print
           :prompt (fn [] false)
           :need-prompt (fn [] false)))
 
-  (shutdown-agents)
-
-  (.flush (.getHistory @jline-reader)))
+  (shutdown-agents))
 
---
+++ src/reply/reader/jline.clj
@@ -0,0 +1,73 @@
+(ns reply.reader.jline
+  (:refer-clojure :exclude [read])
+  (:use [clojure.main :only [repl-read repl-exception]])
+  (:require [reply.completion.jline :as completion.jline])
+  (:import [java.io File IOException PrintStream ByteArrayOutputStream]
+           [reply JlineInputReader]
+           [reply.hacks CustomizableBufferLineNumberingPushbackReader]
+           [scala.tools.jline.console ConsoleReader]
+           [scala.tools.jline.console.history FileHistory]
+           [scala.tools.jline.internal Log]))
+
+(def jline-reader (atom nil))
+(def jline-pushback-reader (atom nil))
+
+(defn make-reader []
+  (let [reader (ConsoleReader.)
+        home (System/getProperty "user.home")
+        history (FileHistory. (File. home ".jline-reply.history"))
+        completer (completion.jline/make-var-completer 'clojure.core)
+        completion-handler (completion.jline/make-completion-handler)]
+    (doto reader
+      (.setBellEnabled false)
+      (.setHistory history)
+      (.setCompletionHandler completion-handler)
+      (.addCompleter completer))))
+
+(defn get-prompt [ns]
+  (format "%s=> " (ns-name ns)))
+
+(defn set-empty-prompt []
+  (.setPrompt
+    @jline-reader
+    (apply str
+           (concat (repeat (- (count (get-prompt *ns*)) 2) \space)
+                   [\| \space]))))
+
+(defn setup-reader! []
+  (Log/setOutput (PrintStream. (ByteArrayOutputStream.)))
+  (reset! jline-reader (make-reader)) ; since construction is side-effect-y
+  (reset! jline-pushback-reader ; since this depends on jline-reader
+    (CustomizableBufferLineNumberingPushbackReader.
+      (JlineInputReader.
+        {:jline-reader @jline-reader
+         :set-empty-prompt set-empty-prompt})
+      1)))
+
+(defn reset-reader []
+  (-> (.getCursorBuffer @jline-reader)
+      (.clear)))
+
+(defn actual-read [input-reader request-prompt request-exit]
+  (binding [*in* input-reader]
+    (repl-read request-prompt request-exit)))
+
+(defn read [request-prompt request-exit]
+  (when-not @jline-reader (setup-reader!))
+  (try
+    (.flush (.getHistory @jline-reader))
+    (.setPrompt @jline-reader (get-prompt *ns*))
+    (let [completer (first (.getCompleters @jline-reader))]
+      (.removeCompleter @jline-reader completer)
+      (.addCompleter @jline-reader (completion.jline/make-var-completer *ns*)))
+    (let [input-stream @jline-pushback-reader]
+      (do
+        (Thread/interrupted) ; just to clear the status
+        (actual-read input-stream request-prompt request-exit)))
+    ; NOTE: this indirection is for wrapped exceptions in 1.3
+    (catch Throwable e
+      (if (#{IOException InterruptedException} (type (repl-exception e)))
+        (do
+          (reset-reader)
+          request-prompt)
+        (throw e)))))
