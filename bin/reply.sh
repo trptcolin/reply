17536c8ff4d6588796898f806a270024c7e154fd
Lee Hinman <lee@writequit.org>
2012-01-17 08:43:43 -0700

    Allow using reply.sh script from any directory

--- bin/reply.sh
+++ bin/reply.sh
@@ -1,4 +1,13 @@
-java -cp lib/clojure-1.3.0.jar:lib/jline-2.9.1.jar:lib/jansi-1.4.jar:lib/clojure-complete-0.1.4.jar:src/:classes/ clojure.main -m reply.main
+#!/bin/sh
+
+BASEDIR=$(dirname $0)
+
+java -cp $BASEDIR/../lib/clojure-1.3.0.jar:\
+$BASEDIR/../lib/jline-2.9.1.jar:\
+$BASEDIR/../lib/jansi-1.4.jar:\
+$BASEDIR/../lib/clojure-complete-0.1.4.jar:\
+$BASEDIR/../src/:\
+$BASEDIR/../classes/ clojure.main -m reply.main
 
 # debugger
 #java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -cp /home/colin/Projects/clojure/clojure-1.4.0-master-SNAPSHOT.jar:/home/colin/Projects/jline2/target/scala-2.9.0-1/jline_2.9.0-1-2.10.0-SNAPSHOT.jar:lib/jansi-1.4.jar:classes/:src/ clojure.main -m reply.main
