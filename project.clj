a9078680f5f5c0c6927f0690aff277614d64e833
Colin Jones <trptcolin@gmail.com>
2012-01-19 20:16:58 -0600

    Bump version based on moving off Scala's JLine

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reply "0.0.2-SNAPSHOT"
+(defproject reply "0.1.0-SNAPSHOT"
   :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [net.sf.jline/jline "2.6-SNAPSHOT"]
