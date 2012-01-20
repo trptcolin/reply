0dc03339594466f8a4789e092c9147d1ed211280
Colin Jones <trptcolin@gmail.com>
2012-01-19 18:59:23 -0600

    Use the sonatype repo, now that the patches are in

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject reply "0.0.2-SNAPSHOT"
   :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.trptcolin/jline "2.6-SNAPSHOT"]
+                 [net.sf.jline/jline "2.6-SNAPSHOT"]
                  [clojure-complete "0.1.4"]]
   :dev-dependencies [[midje "1.3-alpha4"]
                      [lein-midje "[1.0.0,)"]]
   :aot [reply.reader.jline.JlineInputReader]
+  :repositories {"sonatype"
+                 {:url "http://oss.sonatype.org/content/repositories/snapshots"}}
   :java-source-path "src")
