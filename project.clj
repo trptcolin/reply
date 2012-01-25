37c5b3896fa5464a9ab5f9d59c3f9bf50837af22
Colin Jones <trptcolin@gmail.com>
2012-01-24 18:15:47 -0600

    Exclude ALL THE THINGS

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.thnetos/cd-client "0.3.1" :exclusions [org.clojure/clojure]]
                  [clj-stacktrace "0.2.4"]
                  [clojure-complete "0.1.4" :exclusions [org.clojure/clojure]]]
-  :dev-dependencies [[midje "1.3-alpha4"]
+  :dev-dependencies [[midje "1.3-alpha4" :exclusions [org.clojure/clojure]]
                      [lein-midje "[1.0.0,)"]]
   :aot [reply.reader.jline.JlineInputReader]
   :main reply.ReplyMain
