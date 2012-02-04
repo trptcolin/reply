17ceecc662d325b5c6a5a84d3bd51ae557d0cfa4
Caspar Hasenclever <casphas@nonplacet.net>
2012-02-04 17:40:31 +0100

    make cd-client and dependencies optional at run-time
    * --no-clojuredocs command line flag
    * if flag is present, do not require cd-client and
      intern clojuredocs function in initialization
    * if flag is present, omit cd-client depencies in classpath
      construction

--- bin/reply.sh
+++ bin/reply.sh
@@ -25,10 +25,43 @@ if [ -z "$USER_CP" ]; then
   USER_CP=""
 fi
 
+SKIP_CD=false
+if [[ $@ =~ "--no-clojuredocs" ]]
+then
+	SKIP_CD=true
+fi
+
+CD_DEPS=( cd-client
+	clj-http
+	httpclient
+	httpcore
+	commons-logging
+	commons-codec
+	commons-io
+	cheshire
+	jackson-core-asl
+	jackson-smile 
+)
+
+function is_cd_dep() {
+	local dep=`basename $1`
+	for d in ${CD_DEPS[@]}
+	do
+		if expr ${dep} : ${d} > /dev/null
+		then
+			return 0
+		fi
+	done
+	return 1
+}
+
 CP="$BASEDIR"/../src/clj/:\
 "$BASEDIR"/../classes/
 for j in "$BASEDIR"/../lib/*.jar; do
-  CP=$CP:$j
+	if ! ($SKIP_CD && is_cd_dep $j)
+	then
+		CP=$CP:$j
+	fi
 done
 
 java -cp "$CP":"$USER_CP" reply.ReplyMain "$@"
