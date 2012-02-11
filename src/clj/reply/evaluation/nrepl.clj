3f3a5c9447dbfaea6e6e4d39ddacb6a8caee1ec2
Colin Jones <trptcolin@gmail.com>
2012-02-11 15:45:22 -0600

    Upgrade to nREPL 0.2.0

--- src/clj/reply/evaluation/nrepl.clj
+++ src/clj/reply/evaluation/nrepl.clj
@@ -1,31 +1,41 @@
 (ns reply.evaluation.nrepl
   (:require [clojure.tools.nrepl.cmdline :as nrepl.cmdline]
             [clojure.tools.nrepl :as nrepl]
+            [clojure.tools.nrepl.misc :as nrepl.misc]
+            [clojure.tools.nrepl.server :as nrepl.server]
+            [clojure.tools.nrepl.transport :as nrepl.transport]
             [reply.concurrency]
             [reply.eval-state]
             [reply.initialization]
             [reply.reader.jline :as reader.jline]))
 
-(def current-command (atom (fn ([]) ([x]))))
+(def current-command-id (atom nil))
+(def current-session (atom nil))
+(def current-ns (atom (str *ns*)))
 
-(defn handle-client-interruption! []
+(defn handle-client-interruption! [client]
   (reply.concurrency/set-signal-handler!
     "INT"
-    (fn [sig] (println "^C")
-      (@current-command :interrupt))))
+    (fn [sig] (print "^C") (flush)
+      (when-let [command-id @current-command-id]
+        (client {:op "interrupt" :session @current-session :interrupt-id command-id})))))
 
-(defn execute-with-connection [connection options form]
-  (let [response-fn ((:send connection) form)]
-    (reset! current-command response-fn)
-    (for [{:keys [ns value out err] :as res} (nrepl/response-seq response-fn)]
+(defn execute-with-client [client options form]
+  (let [command-id (nrepl.misc/uuid)
+        response-seq (nrepl/message client {:op "eval" :code form :id command-id :session @current-session})]
+    (reset! current-command-id command-id)
+    (doall (for [{:keys [ns value out err] :as res}
+                   (take-while #(not (some #{"done" "interrupted" "error" "eval-error"} (:status %)))
+                               response-seq)]
       (do
-        (when (= "done" (:status res))
-          (reset! current-command {}))
         (when value ((:value options print) value))
         (when out ((:out options print) out))
         (when err ((:err options print) err))
         (flush)
-        ns))))
+        (when ns (reset! current-ns ns)))))
+    (println)
+    (reset! current-command-id nil)
+    @current-ns))
 
 (defn run-repl
   ([connection] (run-repl connection nil))
@@ -41,51 +51,56 @@
                                 done
                                 (prn e))))]
           (if (= done read-result) nil
-              (recur (last (execute-with-connection
+              (recur (execute-with-client
                        connection
                        options
-                       (pr-str read-result))))))))))
+                       (pr-str read-result)))))))))
 
 (defn get-connection [options]
   (let [port (if-let [attach-port (:attach options)]
                (Integer/parseInt attach-port)
-               (let [[ssocket _] (nrepl/start-server (Integer/parseInt (or (:port options) "0")))]
-                 (.getLocalPort ssocket)))]
-    (nrepl/connect "localhost" port)))
+               (-> (nrepl.server/start-server :port (Integer/parseInt (or (:port options) "0")))
+                   deref
+                   :ss
+                   .getLocalPort))]
+    (nrepl/connect :host (or (:host options) "localhost") :port port)))
 
-(defn adhoc-eval [connection form]
+(defn adhoc-eval [client form]
   (let [results (atom "nil")]
-    (doall (execute-with-connection
-      connection
+    (execute-with-client
+      client
       {:value (partial reset! results)
        :out print
        :err print}
-      (pr-str form)))
+      (pr-str form))
     (read-string @results)))
 
 (defn main
   "Ripped from nREPL. The only changes are in namespacing, running of initial
 code, and options."
   [options]
-  (let [connection (get-connection options)]
+  (let [connection (get-connection options)
+        client (nrepl/client connection 10000)
+        session (nrepl/new-session client)]
+    (reset! current-session session)
     (let [options (assoc options :prompt
                     (fn [ns]
                       (binding [*ns* (create-ns (symbol ns))]
                         (reply.eval-state/set-bindings!))
                       (reader.jline/prepare-for-read
-                        (partial adhoc-eval connection))))
+                        (partial adhoc-eval client))))
           options (if (:color options)
                     (merge options nrepl.cmdline/colored-output)
                     options)]
-      (doall (execute-with-connection
-               connection
+      (execute-with-client
+               client
                options
                (pr-str (list 'do
                          (reply.initialization/construct-init-code options)
                          (reply.initialization/export-definition 'reply.concurrency/set-signal-handler!)
                          '(set-signal-handler! "INT" (fn [s]))
-                         nil))))
+                         nil)))
 
-      (handle-client-interruption!)
-      (run-repl connection options))))
+      (handle-client-interruption! client)
+      (run-repl client options))))
 
