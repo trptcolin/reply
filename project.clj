f2e062f0ceab5cf0b1aa5133b1eaf034d2c72439
Colin Jones <trptcolin@gmail.com>
2012-03-05 21:12:58 -0600

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reply "0.1.0-alpha5"
+(defproject reply "0.1.0-SNAPSHOT"
   :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [jline "2.6"]
