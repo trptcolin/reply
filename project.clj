908ccbfd3bfa14e959df1d5784c5aa55b3dacfcd
Phil Hagelberg <technomancy@gmail.com>
2012-03-30 11:34:15 -0700

    Make project.clj work with Leiningen 2.
    The :exclusions aren't necessary since cd-client and clojure-complete
    both depend on the same version of Clojure as reply does.
    
    Added a :main entry to make `lein run` a bit more convenient.

--- project.clj
+++ project.clj
@@ -4,10 +4,13 @@
                  [org.clojars.trptcolin/jline "2.7-alpha1"]
                  [org.thnetos/cd-client "0.3.4"]
                  [clj-stacktrace "0.2.4"]
-                 [clojure-complete "0.2.1" :exclusions [org.clojure/clojure]]
-                 [org.clojure/tools.nrepl "0.2.0-beta1"]]
+                 [clojure-complete "0.2.1"]
+                 [org.clojure/tools.nrepl "0.2.0-beta4"]]
   :dev-dependencies [[midje "1.3-alpha4" :exclusions [org.clojure/clojure]]
                      [lein-midje "[1.0.0,)"]]
   :aot [reply.reader.jline.JlineInputReader]
   :source-path "src/clj"
-  :java-source-path "src/java")
+  :java-source-path "src/java"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+  :main ^:skip-aot reply.main)
