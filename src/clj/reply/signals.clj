c6fe5f35bbea14bafc71dd37b7d50ea4b93ea639
Colin Jones <trptcolin@gmail.com>
2012-04-07 10:48:11 -0500

    Avoid hard fail on unrecognized signals
    Fixes #37

--- src/clj/reply/signals.clj
+++ src/clj/reply/signals.clj
@@ -1,8 +1,12 @@
 (ns reply.signals)
 
 (defn set-signal-handler! [signal f]
-  (sun.misc.Signal/handle
-    (sun.misc.Signal. signal)
-    (proxy [sun.misc.SignalHandler] []
-      (handle [signal] (f signal)))))
+  (try
+    (sun.misc.Signal/handle
+      (sun.misc.Signal. signal)
+      (proxy [sun.misc.SignalHandler] []
+        (handle [signal] (f signal))))
+    (catch IllegalArgumentException e
+      ; unrecognized signal - CONT on Windows, for instance
+      )))
 
