6190aaa913311e2f8866147cba1f9e90f9759349
Colin Jones <trptcolin@gmail.com>
2012-05-09 19:46:44 -0500

    Bump jline to handle inputrc problems

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject reply "0.1.0-SNAPSHOT"
   :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.trptcolin/jline "2.7-alpha3"]
+                 [org.clojars.trptcolin/jline "2.7-alpha4"]
                  [org.thnetos/cd-client "0.3.4"]
                  [clj-stacktrace "0.2.4"]
                  [org.clojure/tools.nrepl "0.2.0-beta6"]
