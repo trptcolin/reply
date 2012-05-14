dacdb5ba295c7cc0e141b730fb249652c823c4f8
Chris Lappe <chris.lappe@gmail.com>
2012-05-14 02:47:28 -0700

    Added support for ~/.reply_profile which will be load-filed in the user ns if it exists.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reply "0.1.0-SNAPSHOT"
+(defproject reply "0.1.0-SNAPSHOT-clappehack"
   :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.trptcolin/jline "2.7-alpha5"]
