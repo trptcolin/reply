5728be763f195eb81c5821a135b6c72697c7d375
Colin Jones <trptcolin@gmail.com>
2012-05-23 18:32:33 -0500

    Eliminate errors from missing sun.misc.Signal
    On Android, for instance.
    
    fixes #57

--- src/clj/reply/signals.clj
+++ src/clj/reply/signals.clj
@@ -1,12 +1,16 @@
 (ns reply.signals)
 
-(defn set-signal-handler! [signal f]
-  (try
-    (sun.misc.Signal/handle
-      (sun.misc.Signal. signal)
-      (proxy [sun.misc.SignalHandler] []
-        (handle [signal] (f signal))))
-    (catch IllegalArgumentException e
-      ; unrecognized signal - CONT on Windows, for instance
-      )))
+(defmacro set-signal-handler! [signal f]
+  (if (try (Class/forName "sun.misc.Signal")
+          (catch Throwable e))
+    `(try
+      (sun.misc.Signal/handle
+        (sun.misc.Signal. ~signal)
+        (proxy [sun.misc.SignalHandler] []
+          (handle [signal#] (~f signal#))))
+      (catch Throwable e#)
+      (catch IllegalArgumentException e#
+        ; unrecognized signal - CONT on Windows, for instance
+        ))
+    `(println "Unable to set signal handlers.")))
 
