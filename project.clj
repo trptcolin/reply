3f3df4cbf62f8a544607d8682ab80a0e5b3e32fc
Colin Jones <trptcolin@gmail.com>
2012-06-01 17:48:06 -0500

    Get dev dependencies lein2-compatible

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
                  [org.clojure/tools.nrepl "0.2.0-beta6"]
                  [com.cemerick/drawbridge "0.0.3"]
                  [clojure-complete "0.2.1"]]
-  :dev-dependencies [[midje "1.3-alpha4" :exclusions [org.clojure/clojure]]
-                     [lein-midje "[1.0.0,)"]]
+  :profiles {:dev {:dependencies
+                    [[midje "1.3-alpha4" :exclusions [org.clojure/clojure]]
+                     [lein-midje "[1.0.0,)"]]}}
   :aot [reply.reader.jline.JlineInputReader]
   :source-path "src/clj"
   :java-source-path "src/java"
