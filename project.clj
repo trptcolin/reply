e64ca4ccf83fde4213a17dfaa169631adbc29121
Colin Jones <trptcolin@gmail.com>
2012-06-23 18:49:21 -0500

    Remove duplication & lein-midje dependency

--- project.clj
+++ project.clj
@@ -1,20 +1,19 @@
-(defproject reply "0.1.0-SNAPSHOT"
-  :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [jline "2.7"]
-                 [org.thnetos/cd-client "0.3.4"]
-                 [clj-stacktrace "0.2.4"]
-                 [org.clojure/tools.nrepl "0.2.0-beta7"]
-                 [com.cemerick/drawbridge "0.0.3"]
-                 [clojure-complete "0.2.1"]]
-  :profiles {:dev {:dependencies
-                    [[midje "1.3-alpha4" :exclusions [org.clojure/clojure]]
-                     [lein-midje "[1.0.0,)"]]}}
-  :dev-dependencies [[midje "1.3-alpha4" :exclusions [org.clojure/clojure]]
-                     [lein-midje "[1.0.0,)"]]
-  :aot [reply.reader.jline.JlineInputReader]
-  :source-path "src/clj"
-  :java-source-path "src/java"
-  :source-paths ["src/clj"]
-  :java-source-paths ["src/java"]
-  :main ^{:skip-aot true} reply.main)
+(let [dev-deps '[[midje "1.3-alpha4" :exclusions [org.clojure/clojure]]]]
+
+  (defproject reply "0.1.0-SNAPSHOT"
+    :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
+    :dependencies [[org.clojure/clojure "1.3.0"]
+                   [jline "2.7"]
+                   [org.thnetos/cd-client "0.3.4"]
+                   [clj-stacktrace "0.2.4"]
+                   [org.clojure/tools.nrepl "0.2.0-beta7"]
+                   [com.cemerick/drawbridge "0.0.3"]
+                   [clojure-complete "0.2.1"]]
+    :profiles {:dev {:dependencies ~dev-deps}}
+    :dev-dependencies ~dev-deps
+    :aot [reply.reader.jline.JlineInputReader]
+    :source-path "src/clj"
+    :java-source-path "src/java"
+    :source-paths ["src/clj"]
+    :java-source-paths ["src/java"]
+    :main ^{:skip-aot true} reply.main))
