cbda024a89eb07e9e84869d22a7ccf92c741450e
Colin Jones <trptcolin@gmail.com>
2012-07-24 09:39:59 -0500

    Move to my jline fork
    While we're waiting for release, this takes care of #67

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   (defproject reply "0.1.0-SNAPSHOT"
     :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
     :dependencies [[org.clojure/clojure "1.3.0"]
-                   [jline "2.7"]
+                   [org.clojars.trptcolin/jline "2.8-SNAPSHOT"]
                    [org.thnetos/cd-client "0.3.4"]
                    [clj-stacktrace "0.2.4"]
                    [org.clojure/tools.nrepl "0.2.0-beta8"]
