3f9fdd3ebab4d0f33acce13a0afa442409fdb1b3
Colin Jones <trptcolin@gmail.com>
2012-10-28 20:46:12 -0500

    Bump sjacket to a non-snapshot

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                    [com.cemerick/drawbridge "0.0.6"]
                    [trptcolin/versioneer "0.1.0"]
                    [clojure-complete "0.2.2"]
-                   [org.clojars.trptcolin/sjacket "0.1.1-SNAPSHOT"
+                   [org.clojars.trptcolin/sjacket "0.1.1"
                     :exclusions [org.clojure/clojure]]]
     :profiles {:dev {:dependencies ~dev-deps}}
     :dev-dependencies ~dev-deps
