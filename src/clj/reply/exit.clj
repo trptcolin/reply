d17b71f2ce3b10b5c54e5ae27e90a29763beb426
Colin Jones <trptcolin@gmail.com>
2012-11-06 07:56:53 -0600

    Fix exit/quit for sjacket parsing (as strings)

--- src/clj/reply/exit.clj
+++ src/clj/reply/exit.clj
@@ -1,7 +1,8 @@
 (ns reply.exit)
 
 (defn done-commands [eof]
-  #{eof 'quit 'exit '(quit) '(exit)})
+  #{eof 'quit 'exit '(quit) '(exit)
+    "quit" "(quit)" "exit" "(exit)"})
 
 (defn done? [eof expression]
   ((done-commands eof) expression))
