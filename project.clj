3e8e71b3e910a2ac1726f6ec9d670044772a551b
Colin Jones <trptcolin@gmail.com>
2013-03-05 10:10:15 -0600

    Bump nrepl version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                    [jline/jline "2.10"]
                    [org.thnetos/cd-client "0.3.6"]
                    [clj-stacktrace "0.2.4"]
-                   [org.clojure/tools.nrepl "0.2.1"]
+                   [org.clojure/tools.nrepl "0.2.2"]
                    [org.clojure/tools.cli "0.2.1"]
                    [com.cemerick/drawbridge "0.0.6"]
                    [trptcolin/versioneer "0.1.0"]
