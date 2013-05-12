6bb6ef281217d3181c7129bb823ab515586ccf3c
Colin Jones <trptcolin@gmail.com>
2013-05-11 20:03:48 -0500

    Add tests actually exercising the completer

--- src/clj/reply/reader/jline/completion.clj
+++ src/clj/reply/reader/jline/completion.clj
@@ -1,26 +1,28 @@
 (ns reply.reader.jline.completion
   (:require [reply.completion :as completion]
-            [reply.eval-state :as eval-state]
             [complete.core])
   (:import [jline.console.completer Completer]))
 
 (defn construct-possible-completions-form [prefix ns]
   `(~'complete.core/completions (~'str ~prefix) (~'symbol ~ns)))
 
+(defn get-prefix [buffer cursor]
+  (let [buffer (or buffer "")]
+    (or (completion/get-word-ending-at buffer cursor) "")))
+
 (defn make-completer [eval-fn redraw-line-fn ns]
   (proxy [Completer] []
     (complete [^String buffer cursor ^java.util.List candidates]
-      ; candidates is empty, wants possibilities added
-      (let [buffer (or buffer "")
-            prefix (or (completion/get-word-ending-at buffer cursor) "")
-            prefix-length (.length prefix)
-            possible-completions-form (construct-possible-completions-form
-                                        prefix ns)
-            possible-completions (eval-fn possible-completions-form)]
-        (if (or (empty? possible-completions) (zero? prefix-length))
+      (let [prefix ^String (get-prefix buffer cursor)
+            prefix-length (.length prefix)]
+        (if (zero? prefix-length)
           -1
-          (do
-            (.addAll candidates possible-completions)
-            (redraw-line-fn)
-            (- cursor prefix-length)))))))
-
+          (let [possible-completions-form (construct-possible-completions-form
+                                            prefix ns)
+                possible-completions (eval-fn possible-completions-form)]
+            (if (empty? possible-completions)
+              -1
+              (do
+                (.addAll candidates possible-completions)
+                (redraw-line-fn)
+                (- cursor prefix-length)))))))))
