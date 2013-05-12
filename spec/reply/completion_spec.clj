416f23808d2e771aeb7a402f1db60f179b0a9db8
Colin Jones <trptcolin@gmail.com>
2013-05-11 19:01:57 -0500

    Refactor & add tests for last-word fetch

--- spec/reply/completion_spec.clj
+++ spec/reply/completion_spec.clj
@@ -3,6 +3,9 @@
         [speclj.core]))
 
 (describe "get-word-ending-at"
+  (it "falls back to the last location if the index is too high"
+    (should= "map" (get-word-ending-at "map" 20)))
+
   (it "finds no words when there's nothing there"
     (should= "" (get-word-ending-at "" 0))
     (should= "" (get-word-ending-at " " 0))
@@ -13,14 +16,29 @@
     (should= "map" (get-word-ending-at "map" 3))
     (should= "map" (get-word-ending-at "(map first [0 1 2])" 4)))
 
-  (it "finds a namespace"
+  (it "finds namespaces, also as symbols or keywords"
     (should= "clojure.c" (get-word-ending-at "clojure.c" 9))
-    (should= "clojure.c" (get-word-ending-at "'clojure.c" 10)))
+    (should= "clojure.c" (get-word-ending-at "'clojure.c" 10))
+    (should= "clojure.c" (get-word-ending-at ":clojure.c" 10)))
 
   (it "omits brackets of several sorts (but takes asterisks)"
-    (should= "" (get-word-ending-at "(map " 5))
+    (should= "*foo" (get-word-ending-at "(*foo" 5))
     (should= "*foo" (get-word-ending-at "[*foo" 5))
-    (should= "*foo" (get-word-ending-at "{*foo" 5)))
+    (should= "*foo" (get-word-ending-at "{*foo" 5))
+    (should= "foo" (get-word-ending-at "]foo" 4))
+    (should= "foo" (get-word-ending-at "}foo" 4))
+    (should= "foo" (get-word-ending-at ")foo" 4)))
+
+  (it "gets nothing when the last segment is a non-word"
+    (should= "" (get-word-ending-at "map " 4))
+    (should= "" (get-word-ending-at "map," 4))
+    (should= "" (get-word-ending-at "map[" 4))
+    (should= "" (get-word-ending-at "map(" 4))
+    (should= "" (get-word-ending-at "map}" 4)))
+
+  (it "omits commas and other whitespace"
+    (should= "foo" (get-word-ending-at ",foo" 4))
+    (should= "foo" (get-word-ending-at ",foo" 4)))
 
   (it "includes slashes (for namespaces and classes)"
     (should= "str/split" (get-word-ending-at "str/split" 9))
--- src/clj/reply/completion.clj
+++ src/clj/reply/completion.clj
@@ -1,22 +1,35 @@
 (ns reply.completion
   (:require [clojure.string :as str]))
 
+(def non-word-chars
+  ["\\s" ","
+   "\\(" "\\)"
+   "\\[" "\\]"
+   "\\{" "\\}"])
+
+(defn input-up-to [input index]
+  (let [topmost-index (min index (.length input))]
+    (.substring input 0 topmost-index)))
+
+(defn not-in-charset [characters]
+  (str "[^" (apply str characters) "]+"))
+
+(defn in-charset [characters]
+  (str "[" (apply str characters) "]+"))
+
+(def word-pattern-string (not-in-charset non-word-chars))
+(def nonword-pattern-string (in-charset non-word-chars))
+
+(defn last-word [input]
+  (-> (str word-pattern-string "|" nonword-pattern-string)
+      re-pattern
+      (re-seq input)
+      last))
+
 (defn get-word-ending-at [input index]
-  (let [start (if (>= (.length input) index)
-                (.substring input 0 index)
-                "")
-        non-word-chars ["\\s" ","
-                        "\\(" "\\)"
-                        "\\[" "\\]"
-                        "\\{" "\\}"]
-        word-pattern-str (str "[^" (apply str non-word-chars) "]+")
-        nonword-pattern-str (str "[" (apply str non-word-chars) "]+")
-        groups (re-seq (re-pattern (str word-pattern-str "|"
-                                        nonword-pattern-str))
-                       start)]
-     (if groups
-       (if-let [last-word (last (re-seq #"[^\s\(\),]+" (last groups)))]
-         (str/replace last-word "'" "")
-         "")
-        "")))
+  (let [input-start (input-up-to input index)
+        last-word (or (last-word input-start) "")]
+    (if (re-seq (re-pattern word-pattern-string) last-word)
+      (str/replace last-word #"[':]" "")
+      "")))
 
