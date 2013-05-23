f0f1f627942e4c6d56a1f28dbf20bc697b004f91
Colin Jones <trptcolin@gmail.com>
2013-05-22 20:11:42 -0500

    Remove redundant catch clause

--- src/clj/reply/signals.clj
+++ src/clj/reply/signals.clj
@@ -4,13 +4,11 @@
   (if (try (Class/forName "sun.misc.Signal")
           (catch Throwable e))
     `(try
-      (sun.misc.Signal/handle
-        (sun.misc.Signal. ~signal)
-        (proxy [sun.misc.SignalHandler] []
-          (handle [signal#] (~f signal#))))
-      (catch Throwable e#)
-      (catch IllegalArgumentException e#
-        ; unrecognized signal - CONT on Windows, for instance
-        ))
+       (sun.misc.Signal/handle
+         (sun.misc.Signal. ~signal)
+         (proxy [sun.misc.SignalHandler] []
+           (handle [signal#] (~f signal#))))
+       ; unrecognized signal - CONT on Windows, for instance
+       (catch Throwable e#))
     `(println "Unable to set signal handlers.")))
 
