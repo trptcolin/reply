682f4cdb0b14f0aef787f35bdb08ef04d796b96e
Colin Jones <trptcolin@gmail.com>
2014-06-10 05:59:44 +0800

    Bump to 0.3.1 for jline & other fixes

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (let [dev-deps '[[speclj "2.7.2"]
                  [classlojure "0.6.6"]]]
 
-  (defproject reply "0.3.1-SNAPSHOT"
+  (defproject reply "0.3.1"
     :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
     :dependencies [[org.clojure/clojure "1.4.0"]
                    [jline "2.12"]
