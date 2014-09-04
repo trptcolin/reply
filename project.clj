f67c47878c730e8e6102e2a80ed12dbe2510b13d
Colin Jones <trptcolin@gmail.com>
2014-09-04 09:26:05 -0500

    Bump nrepl version for NREPL-65

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                    [jline "2.12"]
                    [org.thnetos/cd-client "0.3.6"]
                    [clj-stacktrace "0.2.7"]
-                   [org.clojure/tools.nrepl "0.2.4"]
+                   [org.clojure/tools.nrepl "0.2.5"]
                    [org.clojure/tools.cli "0.3.1"]
                    [com.cemerick/drawbridge "0.0.6"
                     :exclusions [org.clojure/tools.nrepl]]
