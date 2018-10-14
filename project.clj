da094a614533afa3f565521823793ce28bf5dba8
Bozhidar Batsov <bozhidar@batsov.com>
2018-10-14 11:09:35 +0200

    Bump the nREPL dep

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                    [jline "2.14.6"]
                    [org.thnetos/cd-client "0.3.6"]
                    [clj-stacktrace "0.2.7"]
-                   [nrepl "0.4.2"]
+                   [nrepl "0.4.5"]
                    [org.clojure/tools.cli "0.3.1"]
                    [nrepl/drawbridge "0.1.0"]
                    [trptcolin/versioneer "0.1.1"]
