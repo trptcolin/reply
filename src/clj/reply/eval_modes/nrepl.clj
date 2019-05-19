trptcolin/reply

commit 7fe67e4e2eb5580e42da6662fd05cd4c62a6533c

Update some legacy nREPL code
* server is no longer something you can deref (it's a regular record)
* passing a nil port is the same as passing 0

src/clj/reply/eval_modes/nrepl.clj
@@ -155,11 +155,9 @@
 
 (defn get-connection [{:keys [attach host port]}]
   (let [server (when-not attach
-                 (nrepl.server/start-server
-                   :port (Integer/parseInt (str (or port 0)))))
+                 (nrepl.server/start-server :port port))
         port (when-not attach
-               (let [^ServerSocket socket (-> server deref :ss)]
-                 (.getLocalPort socket)))
+               (:port server))
         url (url-for attach host port)]
     (when server
       (reset! nrepl-server server))

spec/reply/integration_spec.clj
@@ -140,6 +140,7 @@
   (around [f]
     (let [cl (classlojure/classlojure
                (str "file:" (:local-path logging))
+               (str "file:" (:local-path bencode))
                (str "file:" (:local-path nrepl))
                (str "file:" (:local-path clojure)))
           server-port

commit 3efcbd59841fda9674c7ccc7e0d2b54ddf1859af

Restore parsing of :port option in nREPL mode
Fixes a regression introduced in 7fe67e4e2eb5580e42da6662fd05cd4c62a6533c.

src/clj/reply/eval_modes/nrepl.clj
@@ -156,7 +156,9 @@
 (defn get-connection [{:keys [attach host port scheme]
                        :or {scheme "nrepl"}}]
   (let [server (when-not attach
-                 (nrepl.server/start-server :port port))
+                 (nrepl.server/start-server
+                   :port (when port
+                           (-> port str Integer/parseInt))))
         port (when-not attach
                (:port server))
         url (url-for attach host port scheme)]