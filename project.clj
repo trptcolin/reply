f922224b0d5e9ddf713014c847560f271bd9c17a
Bozhidar Batsov <bozhidar@batsov.com>
2020-04-08 17:55:09 +0300

    Bump the Clojure dep
    nREPL depends on Clojure 1.7.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 
   (defproject reply "0.4.5-SNAPSHOT"
     :description "REPL-y: A fitter, happier, more productive REPL for Clojure."
-    :dependencies [[org.clojure/clojure "1.6.0"]
+    :dependencies [[org.clojure/clojure "1.7.0"]
                    [jline "2.14.6"]
                    [org.thnetos/cd-client "0.3.6"]
                    [clj-stacktrace "0.2.8"]
