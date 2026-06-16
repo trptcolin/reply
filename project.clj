2b28587004aa3b5cd5d4548eb87f7674eb999b13
Bozhidar Batsov <bozhidar@batsov.dev>
2026-06-16 12:09:36 +0300

    Stop signing Clojars releases
    No GPG key is available in our release environment, so disable artifact
    signing rather than block deploys on it.

--- project.clj
+++ project.clj
@@ -26,4 +26,4 @@
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
-                                    :sign-releases true}]])
+                                    :sign-releases false}]])
